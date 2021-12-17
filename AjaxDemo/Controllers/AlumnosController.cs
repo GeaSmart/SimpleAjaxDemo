83b155aba6fbec1d03dd034c286dbfc9231f4f5d
Gerson Azabache <gerson_dunk@hotmail.com>
2021-12-17 16:43:25 -0500

    initial commit

---
+++ AjaxDemo/Controllers/AlumnosController.cs
@@ -0,0 +1,22 @@
+﻿using AjaxDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AjaxDemo.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddAlumno(Alumno alumno)
+        {
+            //return Json(alumno, JsonRequestBehavior.AllowGet);
+            return Json(alumno, new Newtonsoft.Json.JsonSerializerSettings());
+        }
+    }
+}
---
+++ AjaxDemo/Models/Alumno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AjaxDemo.Models
+{
+    public class Alumno
+    {
+        public int Id { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Aula { get; set; }
+    }
+}
